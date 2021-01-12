c8fe20fa4666a76602aca1560dda54504834dbcb
alexhauka <alexhauka@gmail.com>
2021-01-12 22:06:46 +0000

    implemented spinner as a function which can also take a parameter at call for how many times you'd like it to spin

---
+++ spinner2.js
@@ -0,0 +1,16 @@
+const spinner = function(rotations) {
+  let spin = "|/-\\|/-\\|"; // --> string of spin positions
+  rotations *= 9; // --> 9 is the number of positions for spinner as shown above
+  let time = 100;
+  for (let i = 0, j = 0; i < rotations; i++, j++) {
+    if (j === spin.length) { // --> if it counts outside of the length of spin, it resets that variable to wrap around
+      j = 0; // --> moment of wrap
+    } else {
+      setTimeout(() => {
+        process.stdout.write(`\r${spin[j]} `);
+      }, time += 200);
+    }
+  }
+};
+
+spinner(10); // --> call function passing param as number of full rotations you'd like
